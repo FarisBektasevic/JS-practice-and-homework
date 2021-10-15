94940aafe6986ec731089c2f7ad15aa1b2ea82a5
FarisBektasevic <bektasevicfaris@gmail.com>
2021-10-16 01:22:22 +0200

    js

--- scramblies.js
+++ scramblies.js
@@ -1,4 +1,3 @@
-
 // Scramblies
 // Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
 
@@ -8,31 +7,10 @@
 // Performance needs to be considered
 // Input strings s1 and s2 are null terminated.
 
-// let str1 = 'jscripts'
-// let str2 = 'javascript'
-
-function scramble(str1, str2) {
-    let array1 = str1.split("").sort();
-    let array2 = str2.split("").sort();
-    let index = 0;
-
-    for( var i = 0; index < array2.length && i <= array1.length; i++) {
-        if(array2[i] === array1[i]) {
-            index++;
-        }
-    } 
-
-    if (i <= array1.length){
-        return true
-    } else {
-        return false 
-    }
+const scramble = (str1, str2) => {
+  return [...str2].every(
+    (val) => str2.split(val).length <= str1.split(val).length
+  )
 }
 
-console.log(scramble('cedewaraaossoqqyt','codewars')) // true
-console.log(scramble('katas','steak')) // false 
-console.log(scramble('scriptjava','javascript')) // true
-console.log(scramble('scriptingjava','javascript')) // true
-console.log(scramble('scriptsjava','javascripts')) // true
-console.log(scramble('jscripts','javascript')) // false
-console.log(scramble('aabbcamaomsccdd','commas')) // true
+console.log(scramble('katas', 'steak'))
